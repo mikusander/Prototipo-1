23f0015aadb05f70263b9052608b9e167c4f5467
mikusander <michelangelocrea6@gmail.com>
2025-01-27 16:21:30 +0100

    prime implementazioni del nuovo sistema di gameplay

--- Assets/Script/ControlNewGameplay.cs
+++ Assets/Script/ControlNewGameplay.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class Domanda
+{
+    public string Tipo;
+    public string Contenuto;
+    public bool Risposta;
+}
+
+[System.Serializable]
+public class Domande
+{
+    public Domanda[] domande;
+}
 
 public class ControlNewGameplay : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Nome del file senza estensione (Unity gestirà la cartella Resources)
+    public string fileName = "Easy/DomandeRisposte";
+
+    // Variabile per memorizzare la lista di domande
+    private Domande domande;
+
     void Start()
     {
-        
+        // Carica le domande dal file JSON
+        CaricaDomande();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CaricaDomande()
     {
-        
+        // Carica il file JSON come TextAsset dalla cartella Resources/Easy
+        TextAsset fileJSON = Resources.Load<TextAsset>(fileName);
+
+        // Verifica se il file è stato caricato correttamente
+        if (fileJSON != null)
+        {
+            // Deserializza il contenuto JSON
+            domande = JsonUtility.FromJson<Domande>(fileJSON.text);
+
+            // Stampa tutte le domande nella console
+            foreach (var domanda in domande.domande)
+            {
+                Debug.Log("Tipo: " + domanda.Tipo);
+                Debug.Log("Contenuto: " + domanda.Contenuto);
+                Debug.Log("Risposta: " + domanda.Risposta);
+            }
+        }
+        else
+        {
+            Debug.LogError("File non trovato nella cartella Resources!");
+        }
     }
 }
